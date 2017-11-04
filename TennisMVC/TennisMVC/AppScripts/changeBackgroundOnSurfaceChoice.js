b48d196e809fa4e3ebb0436751210a1ee3e2651a
nasko90 <nasko_drenovichki@abv.bg>
2017-11-04 16:36:56 +0200

    Change background picture method

---
+++ TennisMVC/TennisMVC/AppScripts/changeBackgroundOnSurfaceChoice.js
@@ -0,0 +1,19 @@
+﻿$("#surfaceDropDown").change(function () {
+    debugger;   switch (this.value) {
+        case "Grass":
+            document.getElementById("backGroundPicture").style.backgroundImage = "url('../Images/grass.jpg')";
+            break;
+        case "Clay":
+            document.getElementById("backGroundPicture").style.backgroundImage = "url('../Images/clay.jpg')";
+            break;
+        case "Indoor":
+            document.getElementById("backGroundPicture").style.backgroundImage = "url('../Images/indoor.jpg')";
+            break;
+        case "Hard":
+            document.getElementById("backGroundPicture").style.backgroundImage = "url('../Images/hard.jpg')";
+            break;
+        default:
+            document.getElementById("backGroundPicture").style.backgroundImage = "url('../Images/images.jpg')";
+            break;
+    } 
+});
